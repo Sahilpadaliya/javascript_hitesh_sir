804c07f5ec5db19ec187581717f18f1ed34aad38
Sahil Padaliya <115575373+Sahilpadaliya@users.noreply.github.com>
2024-08-25 04:58:50 +0000

    comparision

---
+++ basic/lec6.js
@@ -0,0 +1,29 @@
+// -------------COMPARISION--------------------------
+
+//  comparision with the same datatype value 
+
+console.log(2>1);
+console.log(2==1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2!=1);
+
+// comparision with the different data types 
+// comparison with the other datatyes is avoidable 
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// strict equality check 
+
+console.log("2"==2);// assignment operator check only the value and not the datatypes of the strict equality
+
+console.log("2"===2);// strict equality check here datatype and the value is check here 
