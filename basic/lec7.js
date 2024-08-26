cdd928df9a006f1dce8b1da045603b382a99da2d
Sahil Padaliya <115575373+Sahilpadaliya@users.noreply.github.com>
2024-08-26 13:56:30 +0000

    commit

--- basic/lec7.js
+++ basic/lec7.js
@@ -2,3 +2,51 @@
 // the  two categories of the dataypes is done on the basis of 
 // 1) data kaise store hote hai 
 // 2) data ko kaise access kr pate hai 
+
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+//array
+const heros = ["shaktiman", "naagraj", "doga"];
+//  object 
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+//function
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+console.log(typeof heros)// array  type is object 
+console.log(typeof myObj)// object type is object
+console.log(typeof myFunction )// function tupe is object function
+
+//null type is object 
+console.log(typeof outsideTemp )// null type is object 
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
