1751e413ffdc9b46eca9db534b226ba3fe6a740c
Sahil Padaliya <115575373+Sahilpadaliya@users.noreply.github.com>
2024-08-30 17:15:54 +0000

    scope

---
+++ basics_03/02_scope.js
@@ -0,0 +1,25 @@
+
+// simply declaring the variable and use it 
+// let a =10;
+// const b =13;
+// var c = 9;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// ----------------------------------------------------------
+// scope of the variable is where we can use the variable upto to the which area 
+
+if(true){
+let a =10;
+const b =13;
+var c = 9;
+}
+// console.log(a);
+// console.log(b);
+console.log(c);// var scope is global therefore the value inside the block scope is accessible and therefore we dont use it
+ 
+
+
+
