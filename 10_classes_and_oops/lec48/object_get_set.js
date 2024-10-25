72e050c968b07ef13421e10315d73c004b860253
Sahilpadaliya <sahilpadaliya36@gmail.com>
2024-10-25 15:38:13 +0530

    get_set_inobj

---
+++ 10_classes_and_oops/lec48/object_get_set.js
@@ -0,0 +1,18 @@
+const user ={
+    _email:"sahil@gmail.com",
+    _password:"abc",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+    set email(value){
+        this._email=value
+
+    }
+
+}
+const tea = Object.create(user)//   ye ek object factory hai jaise class new keyword se 
+                               // empty object milta hai waise hi isse bhi empty object 
+                               // object ke ander milega
+
+console.log(tea.email)                               
